763ce25f7823436d764b13ce1dcc1934a7460e62
Shah Solayman Sinha <md.sinha.p@gmail.com>
2025-09-07 22:55:53 +0600

    Migrate create for Oregon region

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250907_164234 from './20250907_164234';
+import * as migration_20250907_165515 from './20250907_165515';
 
 export const migrations = [
   {
-    up: migration_20250907_164234.up,
-    down: migration_20250907_164234.down,
-    name: '20250907_164234'
+    up: migration_20250907_165515.up,
+    down: migration_20250907_165515.down,
+    name: '20250907_165515'
   },
 ];
