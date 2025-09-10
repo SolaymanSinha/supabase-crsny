8bafa62f48e41bc09b3cf2dbf599ef5f621d72d5
Shah Solayman Sinha <md.sinha.p@gmail.com>
2025-09-10 06:51:33 +0600

    Add dynamic metadata to root layout

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -8,10 +8,34 @@ import { Company } from '@/payload-types'
 import { getFeaturedCategories } from '@/functions/category.function'
 import Footer from '@/components/custom/footer'
 import { getWebContent } from '@/functions/webContent.function'
+import { Metadata } from 'next'
+import { generateSEOMetadata } from '@/lib/utils/seo'
+import { headers } from 'next/headers'
 
-export const metadata = {
-  description: 'Store',
-  title: 'We are here to make your printing works easier',
+export async function generateMetadata(): Promise<Metadata> {
+  try {
+    const company = await getCompany()
+    const headersList = await headers()
+    const pathname = headersList.get('x-invoke-path') || '/'
+
+    // Split the pathname by '/', filter out empty segments, and get the last one.
+    const segments = pathname.split('/').filter(Boolean)
+    const lastPathnameSegment = segments.pop() || 'Home' // Default to 'home' for the root path
+    return generateSEOMetadata(
+      {
+        title: lastPathnameSegment,
+        description: 'We are here to make your printing easier',
+        noIndex: true,
+      },
+      company.data,
+    )
+  } catch (error) {
+    console.error('Error generating Root Layout metadata:', error)
+    return {
+      description: 'Store',
+      title: 'we are here to make your printing works easier',
+    }
+  }
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
