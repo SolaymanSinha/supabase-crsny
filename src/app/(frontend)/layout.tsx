6cab83ff1291a22cc003ef296d6f6ae1e9ced364
Shah Solayman Sinha <md.sinha.p@gmail.com>
2025-09-10 06:40:21 +0600

    Modify default metadata for everypage

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -10,8 +10,8 @@ import Footer from '@/components/custom/footer'
 import { getWebContent } from '@/functions/webContent.function'
 
 export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+  description: 'Store',
+  title: 'We are here to make your printing works easier',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
