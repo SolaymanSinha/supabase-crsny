7862bf4155228ee19d774198f417ce1a492ff7d3
Shah Solayman Sinha <md.sinha.p@gmail.com>
2025-09-10 06:34:09 +0600

    Add cart metadata

--- src/app/(frontend)/cart/page.tsx
+++ src/app/(frontend)/cart/page.tsx
@@ -1,6 +1,23 @@
-'use client'
-
 import { Cart } from '@/components/custom/cart'
+import { getCompany } from '@/functions/company.function'
+import { generateSEOMetadata } from '@/lib/utils/seo'
+import { Metadata } from 'next'
+
+export async function generateMetadata(): Promise<Metadata> {
+  try {
+    const company = await getCompany()
+    return generateSEOMetadata(
+      { title: 'Cart', description: 'Explore your shopping cart', noIndex: true },
+      company.data,
+    )
+  } catch (error) {
+    console.error('Error generating Cart page metadata:', error)
+    return {
+      title: 'Cart',
+      description: 'Explore your shopping cart',
+    }
+  }
+}
 
 export default function CartPage() {
   return (
