dae73d31142544f19495d6eb8d2af3f47e311e5a
rishitajahan <140841548+rishitajahan@users.noreply.github.com>
2025-09-23 21:50:36 +0600

    Add crsny-app subdomain for image load - Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -35,6 +35,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'crsny-app.vercel.app',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 }
